7e049bfcd53a8b8ab0801f94f3339d89f8f33509
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-16 00:56:49 +0600

    added no route and meal details compo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import MealDetails from './Pages/Home/MealDetails/MealDetails';
 import Footer from './Pages/SharedComponent/Footer';
 import Header from './Pages/SharedComponent/Header';
+import NoRoute from './Pages/SharedComponent/NoRoute';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
+          <Route path="/meal-details/:mealId" element={<MealDetails />} />
+          <Route path="*" element={<NoRoute />} />
         </Routes>
       </main>
       <Footer />
--- src/Pages/SharedComponent/Meal.js
+++ src/Pages/SharedComponent/Meal.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Meal = ({ meal }) => {
-  const { strMealThumb, strMeal, strInstructions } = meal;
+  const navigate = useNavigate();
+  const { idMeal, strMealThumb, strMeal, strInstructions } = meal;
   return (
     <div className="card w-auto  bg-base-100 shadow-xl">
       <figure className="px-5 pt-5">
@@ -11,7 +13,12 @@ const Meal = ({ meal }) => {
         <h2 className="card-title font-bold">{strMeal}</h2>
         <p>{strInstructions.slice(0, 70)}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Order Now</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => navigate(`/meal-details/${idMeal}`)}
+          >
+            Order Now
+          </button>
           <button className="btn btn-primary">Add to cart</button>
         </div>
       </div>
